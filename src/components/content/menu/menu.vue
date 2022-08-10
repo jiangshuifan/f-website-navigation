7411a1295d296379d99a0af05cd2b2a760212fb2
14926 <1492681276@qq.com>
2022-08-11 02:00:02 +0800

    fix

--- src/components/content/menu/menu.vue
+++ src/components/content/menu/menu.vue
@@ -1,62 +1,65 @@
 .<template>
-      <div class="menu">
-        <div :class="{'menu-origin':true,'menu-close':pagemenustate}" @click="unfoldmenu"></div>
-        <ul class="menu-item">
-          <li @click="unfoldmenu"><router-link to="/home">首页</router-link></li>
-          <li><span @click="handlesocial">社交</span>
-          <transition name="foldmenu">
-                      <ul v-show="socialmenuUnFold" class="social-type">
-                <li><a>CSDN</a></li>
-                <li><a>Bilibili</a></li>
-                <li><a>Zhihu</a></li>
-                <li><a>GitHub</a></li>
-            </ul>
-          </transition>
-          </li @click="unfoldmenu">
-          <li  @click="unfoldmenu"><router-link to="/contact">联系</router-link></li>
-          <li @click="unfoldmenu"><router-link to="/about">关于</router-link></li>
-        </ul>
-      </div>
+  <div class="menu">
+    <div
+      :class="{ 'menu-origin': true, 'menu-close': pagemenustate }"
+      @click="unfoldmenu"
+    ></div>
+    <ul class="menu-item">
+      <li @click="unfoldmenu"><router-link to="/home">首页</router-link></li>
+      <li>
+        <span @click="handlesocial">社交</span>
+        <transition name="foldmenu">
+          <ul v-show="socialmenuUnFold" class="social-type">
+            <li><a>CSDN</a></li>
+            <li><a>Bilibili</a></li>
+            <li><a>Zhihu</a></li>
+            <li><a>GitHub</a></li>
+          </ul>
+        </transition>
+      </li>
+      <li @click="unfoldmenu"><router-link to="/contact">联系</router-link></li>
+      <li @click="unfoldmenu"><router-link to="/about">关于</router-link></li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "page-menu",
+  name: 'page-menu',
   methods: {
     unfoldmenu() {
-      this.$emit("unfold");
+      this.$emit('unfold')
     },
-    handlesocial(){
+    handlesocial() {
       this.socialmenuUnFold = !this.socialmenuUnFold
-    }
+    },
   },
   data() {
     return {
-      socialmenuUnFold:false,
-      pagemenustate:false
+      socialmenuUnFold: false,
+      pagemenustate: false,
     }
   },
-  props:{
-    menustate:{
-      default:false
+  props: {
+    menustate: {
+      default: false,
     },
-    socialmenu:{
-      default:false
-    }
+    socialmenu: {
+      default: false,
+    },
   },
-  watch:{
-    socialmenu(newV){
+  watch: {
+    socialmenu(newV) {
       this.socialmenuUnFold = newV
     },
-    menustate(newV){
+    menustate(newV) {
       this.pagemenustate = newV
-    }
-  }
-};
+    },
+  },
+}
 </script>
 
 <style>
-
 .menu {
   position: fixed;
   right: 0;
@@ -67,30 +70,33 @@ export default {
   background-color: white;
   font-size: 25px;
 }
-.menu-close{
+.menu-close {
   float: left;
   position: relative;
   height: 30px;
   width: 30px;
 }
-.menu-close::before,.menu-close::after,.menu-origin::before,.menu-origin::after{
+.menu-close::before,
+.menu-close::after,
+.menu-origin::before,
+.menu-origin::after {
   position: absolute;
   content: '';
   background-color: black;
   width: 20px;
   height: 2px;
   top: 50%;
-  transform: translate(0,-50%);
+  transform: translate(0, -50%);
   /* top: 50%;
   left: 50%;
   transform: translate(-50%,-50%); */
-  transform-origin: 15px,15px;
+  transform-origin: 15px, 15px;
 }
-.menu-close::before{
+.menu-close::before {
   transform: rotate(45deg);
   animation: 'anticlockwise' 0.6s linear;
 }
-.menu-close::after{
+.menu-close::after {
   transform: rotate(-45deg);
   animation: 'clockwise' 0.6s linear;
 }
@@ -102,25 +108,37 @@ export default {
   margin-bottom: 50px;
   cursor: pointer;
 }
-.social-type{
-    overflow: hidden;
+.social-type {
+  overflow: hidden;
 }
-.social-type>li{
-      margin-top:2px;
-    text-align: right;
+.social-type > li {
+  margin-top: 2px;
+  text-align: right;
 }
-.social-type>li>a{
-    font-size: 14px;
-    color: rgba(80,80,80,0.8);
+.social-type > li > a {
+  font-size: 14px;
+  color: rgba(80, 80, 80, 0.8);
 }
 @keyframes clockwise {
-  0% {transform: rotate(0);}
-  75%{transform: rotate(0);}
-  100%{transform: rotate(-45deg);}
+  0% {
+    transform: rotate(0);
+  }
+  75% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(-45deg);
+  }
 }
 @keyframes anticlockwise {
-  0% {transform: rotate(0);}
-  75%{transform: rotate(0);}
-  100%{transform: rotate(45deg);}
+  0% {
+    transform: rotate(0);
+  }
+  75% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(45deg);
+  }
 }
 </style>
